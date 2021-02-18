652ed177c25153b2706867271c64dd73374a59fc
Luis Videla <luisvid@gmail.com>
2021-02-18 16:38:03 -0300

    CORS fix

--- app.py
+++ app.py
@@ -8,9 +8,18 @@ from resources.echo import Echo
 app = Flask(__name__)
 api = Api(app)
 
+@app.after_request
+
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response
+
 api.add_resource(Echo, "/echo/<string:echo>")
 api.add_resource(Dollar, "/dollar")
 api.add_resource(Cedears, "/cedears")
+
 
 if __name__ == "__main__":
   app.run()
